6b47ff0e9b1ad0a35bedf5af2d16ca1dab7a9fb2
Marton Bakk <marci.bakk2004@gmail.com>
2025-11-21 19:41:44 +0100

    Refactor DataLoader to separate data loading and splitting functionality

--- src/data_loader/data_loader.py
+++ src/data_loader/data_loader.py
@@ -37,9 +37,11 @@ class DataLoader:
         self.__image_data_set_path: str = image_data_set_path
         self.__meta_data_path: str = meta_data_path
         self.__label_info_path: str = label_info_path
-        self.data_set: List[Sample] = self.__load_data()
+        data: Tuple[List[Sample], List[Sample]] = self.__load_data()
+        self.__training_set: List[Sample] = data[0]
+        self.__validation_set: List[Sample] = data[1]
 
-    def __load_data(self) -> List[Sample]:
+    def __load_data(self) -> Tuple[List[Sample], List[Sample]]:
         """Loads the dataset from the specified path.
         Returns:
             Tuple[List[Sample], List[Sample]]: A tuple containing the training and validation datasets.
@@ -105,4 +107,25 @@ class DataLoader:
                     info=info,
                 )
             )
-        return samples
+        return self.__split_data(samples)
+
+    def __split_data(
+        self, samples: List[Sample], validation_split: float = DATA_SPLIT
+    ) -> Tuple[List[Sample], List[Sample]]:
+        """Splits the dataset into training and validation sets.
+        Args:
+            samples (List[Sample]): The list of samples to split.
+            validation_split (float, optional): The proportion of the dataset to include in the validation split. Defaults to 0.2.
+        Returns:
+            Tuple[List[Sample], List[Sample]]: A tuple containing the training and validation datasets.
+        """
+        split_index = int(len(samples) * (1 - validation_split))
+        training_set = samples[:split_index]
+        validation_set = samples[split_index:]
+        return (training_set, validation_set)
+
+    def get_training_set(self) -> Optional[List[Sample]]:
+        return self.__training_set
+
+    def get_validation_set(self) -> Optional[List[Sample]]:
+        return self.__validation_set
